272b503c0fb09f9566b473585596bbcb097f3601
Matthew Wolfe <matthew.wolfe2@gmail.com>
2023-12-28 13:30:33 -0800

    patch: update prompt for context behavior

--- src/context/promptForContext/promptForContext.ts
+++ src/context/promptForContext/promptForContext.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'node:path';
 import inquirer from 'inquirer';
 import { z } from 'zod';
+import { existsSync } from 'node:fs';
 
 interface PromptForContextOptions {
   template: {
@@ -9,28 +10,40 @@ interface PromptForContextOptions {
   };
 }
 
-const promptsSchema = z.array(
-  z.object({
-    message: z.string(),
-    name: z.string(),
-    required: z.boolean(),
-    type: z.union([z.literal('input'), z.literal('select')]),
-  })
+const promptsSchema = z.optional(
+  z.array(
+    z.object({
+      message: z.string(),
+      name: z.string(),
+      required: z.boolean(),
+      type: z.union([z.literal('input'), z.literal('select')]),
+    })
+  )
 );
 
-const helpersSchema = z.record(z.function(z.tuple([z.string()]), z.string()));
+const helpersSchema = z.optional(
+  z.record(z.function(z.tuple([z.string()]), z.string()))
+);
 
 async function promptForContext({
   template: { path, template },
 }: PromptForContextOptions) {
-  const { prompts, helpers } = await import(
-    resolve(`${path}/${template}`, 'context.mjs')
-  );
+  const contextFilePath = resolve(`${path}/${template}`, 'context.mjs');
 
-  const parsedPrompts = promptsSchema.parse(prompts);
-  const parsedHelpers = helpersSchema.parse(helpers);
+  if (!existsSync(contextFilePath)) {
+    return {
+      context: {},
+      helpers: {},
+    };
+  }
 
-  const context = await inquirer.prompt<Record<string, unknown>>(parsedPrompts);
+  const { prompts, helpers } = await import(contextFilePath);
+
+  const parsedPrompts = promptsSchema.parse(prompts) || [];
+  const parsedHelpers = helpersSchema.parse(helpers) || {};
+
+  const context =
+    (await inquirer.prompt<Record<string, unknown>>(parsedPrompts)) || {};
 
   return {
     context,
