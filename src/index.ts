0e54ec4633fae603344efb21f616afb1efc46466
Matthew Wolfe <matthew.wolfe2@gmail.com>
2024-01-04 13:33:47 -0800

    patch: export getAllPaths

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { promptForContext } from '@pkg/context/promptForContext';
 export { promptForTemplate } from '@pkg/templates/promptForTemplate';
 export { generate } from '@pkg/templates/generate';
+export { getAllPaths } from '@pkg/templates/getAllPaths';
 export { getTemplate } from '@pkg/templates/getTemplate';
