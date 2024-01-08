a96308ea01c5412b5c921bb75d2350532a8bd811
Matthew Wolfe <matthew.wolfe2@gmail.com>
2024-01-08 08:31:00 -0800

    patch: export cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   dts: true,
   entry: ['src/index.ts', 'src/cli/plano.ts'],
-  format: 'esm',
+  format: ['cjs', 'esm'],
   minify: 'terser',
   outDir: 'dist',
 });
